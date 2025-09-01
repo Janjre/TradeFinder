502e7800d149c4bc5a21a78d717800149e7b7b6f
Janjre <theshepherds555@gmail.com>
2025-09-01 12:02:52 +0100

    Got more prototypes working, need to check trading but breaking/placing blocks work

--- TradeFinder/TradeFinder.cs
+++ TradeFinder/TradeFinder.cs
@@ -1,5 +1,6 @@
 using OnixRuntime.Api;
 using OnixRuntime.Api.Entities;
+using OnixRuntime.Api.Inputs;
 using OnixRuntime.Api.Maths;
 using OnixRuntime.Api.Options;
 using OnixRuntime.Plugin;
@@ -7,8 +8,65 @@ using OnixRuntime.Api.Rendering;
 using OnixRuntime.Api.UI;
 using OnixRuntime.Api.World;
 using System.Runtime.InteropServices;
+using System.Text.Json;
 
 namespace TradeFinder {
+    using System;
+    using System.Collections.Generic;
+
+    public class MyTask
+    {
+        private long startTime;
+        private long endTime;
+
+        public int Duration { get; private set; } // in ms
+        public Action OnTimeReached { get; private set; }
+        public bool Active { get; private set; }
+
+        public MyTask(Action func, int duration)
+        {
+            startTime = CurrentMillis();
+            endTime = startTime + duration;
+            Duration = duration;
+            Active = true;
+            OnTimeReached = func;
+        }
+
+        public void Update()
+        {
+            if (Active && endTime <= CurrentMillis())
+            {
+                Active = false;
+                OnTimeReached?.Invoke();
+            }
+        }
+
+        private static long CurrentMillis()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+    }
+
+    public static class MyTaskScheduler
+    {
+        private static readonly List<MyTask> ActiveTasks = new List<MyTask>();
+
+        public static void Update()
+        {
+            foreach (var t in ActiveTasks)
+            {
+                if (t.Active)
+                {
+                    t.Update();
+                }
+            }
+        }
+
+        public static void AddTask(MyTask task)
+        {
+            ActiveTasks.Add(task);
+        }
+    }
 
     public class TradeSettings {
         public string Enchant;
@@ -22,6 +80,7 @@ namespace TradeFinder {
 
         public TradeSettings(string enchant) {
             Enchant = enchant;
+            microStage = "stop";
 
             Console.WriteLine("Please run $select on the workstation");
             stage = "asking block";
@@ -30,6 +89,7 @@ namespace TradeFinder {
         public TradeSettings(string enchant, int level) {
             Enchant = enchant;
             Level = level;
+            microStage = "stop";
             
             Console.WriteLine("Please run $select on the workstation");
             stage = "asking block";
@@ -39,6 +99,7 @@ namespace TradeFinder {
             Enchant = enchant;
             Level = level;
             MaxPrice = maxPrice;
+            microStage = "stop";
             
             Console.WriteLine("Please run $select on the workstation");
             stage = "asking block";
@@ -46,6 +107,7 @@ namespace TradeFinder {
 
         public void begin() {
             going = true;
+            microStage = "break";
         }
         
         public void stop() {
@@ -76,9 +138,46 @@ namespace TradeFinder {
             }
         }
 
+        public void lookAtWork() {
+            Onix.LocalPlayer.SetFacingXYZ(workBlock);
+
+        }
+
     }
     public static class Globals {
         public static TradeSettings currentSearch;
+        public static void CubeFrame(RendererWorld gfx,float x, float y, float z, float s)
+        {
+            // shift to center
+            float half = s / 2f;
+            float x0 = x - half, x1 = x + half;
+            float y0 = y - half, y1 = y + half;
+            float z0 = z - half, z1 = z + half;
+
+            // bottom square
+            gfx.DrawLine(new Vec3(x0, y0, z0), new Vec3(x1, y0, z0),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x1, y0, z0), new Vec3(x1, y1, z0),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x1, y1, z0), new Vec3(x0, y1, z0),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x0, y1, z0), new Vec3(x0, y0, z0),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x0, y0, z1), new Vec3(x1, y0, z1),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x1, y0, z1), new Vec3(x1, y1, z1),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x1, y1, z1), new Vec3(x0, y1, z1),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x0, y1, z1), new Vec3(x0, y0, z1),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x0, y0, z0), new Vec3(x0, y0, z1),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x1, y0, z0), new Vec3(x1, y0, z1),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x1, y1, z0), new Vec3(x1, y1, z1),ColorF.Cyan,ColorF.Cyan);
+            gfx.DrawLine(new Vec3(x0, y1, z0), new Vec3(x0, y1, z1),ColorF.Cyan,ColorF.Cyan);
+        }
+
+        public static void exploreChildren(GameUIElement root,int tabbage) {
+            foreach (GameUIElement child in root.Children) {
+                
+                Console.WriteLine($" {string.Concat(Enumerable.Repeat("  ",tabbage ))}Found {child.Name}");
+                exploreChildren(child, tabbage+1);
+                
+            }
+        }
+        
     }
     public class TradeFinder : OnixPluginBase {
         public static TradeFinder Instance { get; private set; } = null!;
@@ -101,6 +200,7 @@ namespace TradeFinder {
             Onix.Events.Common.HudRenderGame += OnHudRenderGame;
             Onix.Events.Common.WorldRender += OnWorldRender;
             Onix.Events.Session.Chat.ChatScreenMessageAboutToBeSent += OnChat;
+            Onix.Events.Input.Input += OnKey;
         }
 
         protected override void OnEnabled() {
@@ -120,23 +220,80 @@ namespace TradeFinder {
             Onix.Events.Common.HudRenderGame -= OnHudRenderGame;
             Onix.Events.Common.WorldRender -= OnWorldRender;
             Onix.Events.Session.Chat.ChatScreenMessageAboutToBeSent -= OnChat;
+            Onix.Events.Input.Input -= OnKey;
+            
+        }
+
+        private bool OnKey(InputKey key, bool isDown) {
+            if (key == InputKey.Type.P) {
+                Globals.exploreChildren(Onix.Gui.RootUiElement,0);
+            }
+            return false;
         }
 
         private void OnTick() {
-            if (Globals.currentSearch.going) {
-                //break workstation
-                
-                // Onix.LocalPlayer.BreakBlock();
+            // Console.WriteLine(Onix.Gui.ScreenName);
+            MyTaskScheduler.Update();
+            if (Globals.currentSearch != null) {
+                if (Globals.currentSearch.going) {
+                    switch (Globals.currentSearch.microStage) {
+                        case "break":
+                            if (Onix.Region.GetBlock(new BlockPos(Globals.currentSearch.workBlock)).Name == "lectern") {
+                                Globals.currentSearch.lookAtWork();
+                                Onix.LocalPlayer.BreakBlock();
+                            } else {
+                                
+                                Globals.currentSearch.microStage = "place";
+                            }
+                            break;
+                        case "place":
+                            Onix.LocalPlayer.BuildBlock(new BlockPos((int)Globals.currentSearch.workBlock.X,
+                                (int)Globals.currentSearch.workBlock.Y, (int)Globals.currentSearch.workBlock.Z), BlockFace.Bottom);
+                            Globals.currentSearch.microStage = "check";
+
+                            break;
+                        case "check":
+                            // Onix.LocalPlayer.Interact(Globals.currentSearch.villager);
+                            if (Onix.Gui.ScreenName != "trade_screen") {
+                                Onix.LocalPlayer.Interact(Globals.currentSearch.villager);
+                            } else { // you are in the trade screen!, must get data then close it
+                                
+                                Onix.Gui.CloseCurrentScreen();
+                            }
+                            
+                            Globals.currentSearch.microStage = "break";
+                            
+                            
+                            break;
+                    
+                    }
+                }
             }
+            
         }
 
         private void OnHudRender(RendererCommon2D gfx, float delta) {
+            // if (Globals.currentSearch != null) {
+            //     gfx.RenderText(new Vec2(0,0),ColorF.White,Globals.currentSearch.going.ToString());
+            //     gfx.RenderText(new Vec2(0,10),ColorF.White, Globals.currentSearch.microStage);    
+            // }
+            
+            
+            
+            
+            
         }
         
         private void OnHudRenderGame(RendererGame gfx, float delta) {
+            
         }
 
         private void OnWorldRender(RendererWorld gfx, float delta) {
+            if (Globals.currentSearch != null) {
+                if (Globals.currentSearch.workBlock != null) {
+                    Globals.CubeFrame(gfx,Globals.currentSearch.workBlock.X,Globals.currentSearch.workBlock.Y,Globals.currentSearch.workBlock.Z,1);
+                }
+            }
             
         }
         
@@ -193,6 +350,7 @@ namespace TradeFinder {
                         case "odds":
                             Console.WriteLine("odds calculator coming soon!");
                             break;
+                        
                         
                     }
                 }
--- TradeFinder/obj/x64/Debug/net8.0/TradeFinder.AssemblyInfo.cs
+++ TradeFinder/obj/x64/Debug/net8.0/TradeFinder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradeFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fb7207309b20261f911f0a04ba04f7b35472d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradeFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradeFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
